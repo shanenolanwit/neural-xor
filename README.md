aae1d5631e17d3e9839315724b2e7ae58272ca3c
Shane Nolan <shane.nolan.wit@gmail.com>
2020-01-26 10:52:53 +0000

    added simple solver solution

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # neural-xor
+
+## Multi-Layer Neural Networks with Sigmoid Function
+
+
+This script is inspired by this [towardsdatascience](https://towardsdatascience.com/multi-layer-neural-networks-with-sigmoid-function-deep-learning-for-rookies-2-bf464f09eb7f) article
+
+## Solving XOR using hidden layer
+Uses a simple neural network with no intelligent back propagation logic. If the correct classification for each input 
+is not identified, all weights and biases are changed at random.
+
+The output lets us know the following : 
+- The number of guesses of required to get the weights and biases correct (There are many correct solutions)
+- Weights and biases for the two hidden layer activation functions and the final activation function.
+- A list comprising of each input and its path through the neural network
+
+## Sample outputs:
+> node index.js  
+found a result after 242 tries  
+23 -27 -18 3 -26 24  
+(0,0) => sig(0\*23 + 0\*23 + -27) sig(0\*-18 + 0\*-18 + 3) => sig(1\*-26 + 0\*-26 + 24) => 0  
+(0,1) => sig(0\*23 + 1\*23 + -27) sig(0\*-18 + 1\*-18 + 3) => sig(1\*-26 + 0\*-26 + 24) => 1  
+(1,0) => sig(1\*23 + 0\*23 + -27) sig(1\*-18 + 0\*-18 + 3) => sig(1\*-26 + 0\*-26 + 24) => 1  
+(1,1) => sig(1\*23 + 1\*23 + -27) sig(1\*-18 + 1\*-18 + 3) => sig(1\*-26 + 0\*-26 + 24) => 0  
+
+> node index.js  
+found a result after 845 tries  
+13 -16 -21 0 -8 0  
+(0,0) => sig(0\*13 + 0\*13 + -16) sig(0\*-21 + 0\*-21 + 0) => sig(1\*-8 + 0\*-8 + 0) => 0  
+(0,1) => sig(0\*13 + 1\*13 + -16) sig(0\*-21 + 1\*-21 + 0) => sig(1\*-8 + 0\*-8 + 0) => 1  
+(1,0) => sig(1\*13 + 0\*13 + -16) sig(1\*-21 + 0\*-21 + 0) => sig(1\*-8 + 0\*-8 + 0) => 1  
+(1,1) => sig(1\*13 + 1\*13 + -16) sig(1\*-21 + 1\*-21 + 0) => sig(1\*-8 + 0\*-8 + 0) => 0  
+
---
+++ index.js
@@ -0,0 +1,91 @@
+/**
+ * Takes an numerical input i
+ * Returns 0 if i is a negative number
+ * Returns 1 is i is a positive number
+ */
+const sigmoid = ( i ) => {
+    return ( i < 0 ) ? 0 : 1;
+}
+
+/**
+ * Math random produces a number between 0 and 1
+ * This is multiplied by 30 to give us a number between 0 and 30
+ * Math floor rounds this down to the nearest integer
+ * Number is then randomly returned as negative or positive
+ */
+const randomNumber = () => {
+    const n = Math.floor(Math.random() * 30);
+    return randomBoolean() ? n*-1 : n;
+}
+
+/**
+ * Math random produces a number between 0 and 1
+ * Return 0 if this number is less than 0.5
+ * Otherwise return 1
+ */
+const randomBoolean = () => {
+    return Math.random() >= 0.5;
+}
+
+/**
+ * Compares to arrays, returns true if they are equal
+ * Otherwise returns false
+ * @param {*} a1 
+ * @param {*} a2 
+ */
+const arraysEqual = (a1, a2) => {
+    return a1.length === a2.length 
+    && a1.every((e, i) => e === a2[i])
+}
+
+
+let count = 0;
+let keepGoing = true;
+// [(0,0),(0,1),(1,0),(1,1)]
+const inputs = [ {x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1} ]
+while(count< 1000 && keepGoing){
+    const weightX = randomNumber();
+    const weightY = randomNumber();
+
+    const biasX = randomNumber();
+    const biasY = randomNumber();
+
+    const weightZ = randomNumber();
+    const biasZ = randomNumber();
+
+    let sig1;
+    let sig2
+    // map each input to the result of it passing through the neural network (0 or 1)
+    const res = inputs.map( input => {
+        const { x, y } = input;
+        // initial inputs passed to hidden layer activation functions
+        const input1 = x*weightX + y*weightX + biasX;
+        const input2 = x*weightY + y*weightY + biasY;
+        sig1 = sigmoid(input1)
+        sig2 = sigmoid(input2)
+        // takes the results from the above and passes it through the final activation function
+        const input3 = sig1*weightZ + sig2*weightZ + biasZ;
+        const sig3 = sigmoid(input3);
+        // returns a 0 or a 1 representing the active status
+        return sig3;
+    });
+
+    // Our inputs into the neural network should result in the following result
+    // (0,0) => 0
+    // (0,1) => 1
+    // (1,0) => 1
+    // (1,1) => 0
+    const xor = [ 0, 1, 1, 0];
+    if(arraysEqual(xor, res)){
+        console.log(`found a result after ${count} tries`)
+        console.log(`${weightX} ${biasX} ${weightY} ${biasY} ${weightZ} ${biasZ}`);
+        console.log(`(0,0) => sig(0*${weightX} + 0*${weightX} + ${biasX}) sig(0*${weightY} + 0*${weightY} + ${biasY}) => sig(${sig1}*${weightZ} + ${sig2}*${weightZ} + ${biasZ}) => 0`);
+        console.log(`(0,1) => sig(0*${weightX} + 1*${weightX} + ${biasX}) sig(0*${weightY} + 1*${weightY} + ${biasY}) => sig(${sig1}*${weightZ} + ${sig2}*${weightZ} + ${biasZ}) => 1`);
+        console.log(`(1,0) => sig(1*${weightX} + 0*${weightX} + ${biasX}) sig(1*${weightY} + 0*${weightY} + ${biasY}) => sig(${sig1}*${weightZ} + ${sig2}*${weightZ} + ${biasZ}) => 1`);
+        console.log(`(1,1) => sig(1*${weightX} + 1*${weightX} + ${biasX}) sig(1*${weightY} + 1*${weightY} + ${biasY}) => sig(${sig1}*${weightZ} + ${sig2}*${weightZ} + ${biasZ}) => 0`);
+        keepGoing = false;
+    }
+    
+    count = count + 1;
+
+}
